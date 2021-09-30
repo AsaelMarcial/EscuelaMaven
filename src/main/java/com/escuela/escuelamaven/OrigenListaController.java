dcc7aec363025e2afea3256277d4be6108e2b6a2
AsaelMarcial <65348664+AsaelMarcial@users.noreply.github.com>
2021-09-30 17:02:56 -0500

    CRUD Origenes

---
+++ src/main/java/com/escuela/escuelamaven/OrigenListaController.java
@@ -0,0 +1,144 @@
+package com.escuela.escuelamaven;
+
+import db.OrigenDAO;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import pojos.OrigenPOJO;
+
+
+public class OrigenListaController {
+    @FXML
+    private ListView<OrigenPOJO> listvOrigenes;
+    @FXML
+    private Button btnActualizar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private TextField txfOrigen;
+    @FXML
+    private Button btnRegresar;
+    @FXML
+    private Label lblPruebaSQL;
+    @FXML
+    private Button btnRegistrar;
+    @FXML
+    private Button btnBuscar;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        listvOrigenes.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        actualizarListaOrigenes();
+    }
+
+    private void actualizarListaOrigenes() {
+        OrigenDAO origenDAO = new OrigenDAO();
+        this.listvOrigenes.setItems(origenDAO.ObtenerOrigenes());
+    }
+
+    @FXML
+    private void btnRegistrar_Click(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("OrigenFormularioView.fxml"));
+        Scene scene = new Scene(root);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Registrar origen");
+        stage.showAndWait();
+
+        actualizarListaOrigenes();
+    }
+
+    @FXML
+    private void btnActualizar_Click(ActionEvent event) throws IOException {
+        OrigenPOJO origenSeleccionado = new OrigenPOJO();
+        if (listvOrigenes.getSelectionModel().getSelectedItem() != null) {
+            origenSeleccionado = listvOrigenes.getSelectionModel().getSelectedItem();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("OrigenFormularioFXML.fxml"));
+            Parent root = loader.load();
+            OrigenFormularioController controlador = loader.getController();
+            controlador.setDatos(origenSeleccionado);
+
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setTitle("Actualizar origen");
+            stage.showAndWait();
+
+            actualizarListaOrigenes();
+        } else {
+            JOptionPane.showMessageDialog(null, "Seleccione un origen de la lista");
+        }
+    }
+
+    @FXML
+    private void btnEliminar_Click(ActionEvent event) {
+        if (listvOrigenes.getSelectionModel().getSelectedItem() != null) {
+            OrigenPOJO origen = new OrigenPOJO();
+            origen = listvOrigenes.getSelectionModel().getSelectedItem();
+
+            OrigenDAO origenDAO = new OrigenDAO();
+            boolean tieneEstudiantes = origenDAO.TieneEstudiantes(origen.getIdOrigen());
+
+            int confirmacion = JOptionPane.showConfirmDialog(null, "¿Seguro que desea eliminarlo?");
+
+            if(confirmacion == 0 && !tieneEstudiantes){
+                origenDAO.EliminarOrigen(origen.getIdOrigen());
+            }else if(tieneEstudiantes){
+                JOptionPane.showMessageDialog(null, "No se puede eliminar el origen por que tiene estudiantes asignados");
+            }
+
+            actualizarListaOrigenes();
+        } else {
+            JOptionPane.showMessageDialog(null, "Seleccione un origen de la lista");
+        }
+    }
+
+
+    @FXML
+    private void btnRegresar_Click(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("MenuPrincipalFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Menu principal");
+        Stage stageActual = (Stage) btnRegresar.getScene().getWindow();
+        stageActual.close();
+        stage.show();
+    }
+
+    @FXML
+    private void btnBuscar_Click(ActionEvent event) {
+        String buscar = txfOrigen.getText();
+        if(buscar.length() != 0){
+            OrigenDAO origenDAO = new OrigenDAO();
+            this.listvOrigenes.setItems(origenDAO.ObtenerOrigenesPorEstado(txfOrigen.getText()));
+        }else{
+            actualizarListaOrigenes();
+        }
+    }
+}
---
+++ src/main/java/db/OrigenDAO.java
@@ -0,0 +1,220 @@
+package db;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import pojos.OrigenPOJO;
+import static db.Conexion.ConectarBD;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OrigenDAO {
+    public ObservableList<OrigenPOJO> ObtenerOrigenes() {
+        ObservableList<OrigenPOJO> listaOrigenes = FXCollections.observableArrayList();
+        String consulta = null;
+        try {
+            Connection conn = ConectarBD();
+
+            if (conn != null) {
+                consulta = "SELECT * FROM origen";
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next() == true) {
+                    OrigenPOJO o = new OrigenPOJO();
+                    o.setIdOrigen(rs.getInt("idorigen"));
+                    o.setEstado(rs.getString("estado"));
+                    o.setCiudad(rs.getString("ciudad"));
+
+                    listaOrigenes.add(o);
+                }
+
+                rs.close();
+                ps.close();
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error de consulta en la base de datos: " + e.getMessage());
+        }
+
+        return listaOrigenes;
+    }
+
+    public static void RegistrarOrigen(String estado, String ciudad) {
+        String consulta;
+
+        try {
+            Connection conn = ConectarBD();
+
+            if (conn != null) {
+                consulta = "INSERT INTO origen (estado, ciudad) VALUES (?, ?)";
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ps.setString(1, estado);
+                ps.setString(2, ciudad);
+
+                ps.executeUpdate();
+                ps.close();
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error de registro en la base de datos: " + e.getMessage());
+        }
+    }
+
+    public Boolean ExisteOrigen(String estado, String ciudad) {
+        ArrayList<OrigenPOJO> listaOrigenes = new ArrayList<OrigenPOJO>();
+        String consulta = null;
+        try {
+            Connection conn = ConectarBD();
+
+            if (conn != null) {
+                consulta = "SELECT * FROM origen WHERE estado= ? AND ciudad= ? ;";
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ps.setString(1, estado);
+                ps.setString(2, ciudad);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next() == true) {
+                    OrigenPOJO o = new OrigenPOJO();
+                    o.setIdOrigen(rs.getInt("idorigen"));
+                    o.setEstado(rs.getString("estado"));
+                    o.setCiudad(rs.getString("ciudad"));
+
+                    listaOrigenes.add(o);
+                }
+
+                if(!listaOrigenes.isEmpty()){
+                    return true;
+                }
+
+                rs.close();
+                ps.close();
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error de consulta en la base de datos: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+    public static void ActualizarOrigen(int idOrigen, String estado, String ciudad) {
+        String consulta;
+
+        try {
+            Connection conn = ConectarBD();
+
+            if (conn != null) {
+                consulta = "UPDATE origen SET estado=?, ciudad=? WHERE idorigen = " + idOrigen;
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ps.setString(1, estado);
+                ps.setString(2, ciudad);
+
+                ps.executeUpdate();
+                ps.close();
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error de actualización en la base de datos: " + e.getMessage());
+        }
+    }
+
+    public void EliminarOrigen(int idOrigen) {
+        String consulta;
+        try {
+            Connection conn = ConectarBD();
+            if (conn != null) {
+                consulta = "DELETE FROM origen WHERE idorigen = " + idOrigen;
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ps.executeUpdate();
+
+                ps.close();
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error de eliminación en la base de datos: " + e.getMessage());
+        }
+    }
+
+    public ObservableList<OrigenPOJO> ObtenerOrigenesPorEstado(String estado) {
+        ObservableList<OrigenPOJO> listaOrigenes = FXCollections.observableArrayList();
+        String consulta = null;
+        try {
+            Connection conn = ConectarBD();
+
+            if (conn != null) {
+                consulta = "SELECT * FROM origen WHERE estado= ? ";
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ps.setString(1, estado);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next() == true) {
+                    OrigenPOJO o = new OrigenPOJO();
+                    o.setIdOrigen(rs.getInt("idorigen"));
+                    o.setEstado(rs.getString("estado"));
+                    o.setCiudad(rs.getString("ciudad"));
+
+                    listaOrigenes.add(o);
+                }
+
+                rs.close();
+                ps.close();
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error de consulta en la base de datos: " + e.getMessage());
+        }
+
+        return listaOrigenes;
+    }
+
+
+    public boolean TieneEstudiantes(int idOrigen) {
+        String consulta;
+        ObservableList<EstudiantePOJO> listaEstudiantes = FXCollections.observableArrayList();
+
+        try {
+            Connection conn = ConectarBD();
+
+            if (conn != null) {
+                consulta = "SELECT * FROM estudiante WHERE origen_idorigen= " + idOrigen;
+                PreparedStatement ps = conn.prepareStatement(consulta);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next() == true) {
+                    EstudiantePOJO e = new EstudiantePOJO();
+                    e.setPrimer_nom(rs.getString("primer_nom"));
+                    e.setPrimer_ape(rs.getString("primer_ape"));
+                    e.setSeg_nom(rs.getString("seg_nom"));
+                    e.setSegundo_ape(rs.getString("seg_ape"));
+                    e.setActivo(rs.getBoolean("activo"));
+                    e.setIdColegio(rs.getInt("colegio_idColegio"));
+                    e.setHistorial(rs.getString("historial"));
+                    e.setIdOrigen(rs.getInt("origen_idOrigen"));
+
+                    listaEstudiantes.add(e);
+
+                }
+
+                rs.close();
+                ps.close();
+                conn.close();
+            }
+
+            if(!listaEstudiantes.isEmpty()){
+                return true;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error de actualización en la base de datos: " + e.getMessage());
+        }
+
+        return false;
+    }
+}
